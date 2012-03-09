75d029cb616f7f07c8b5578395f1965f9d62ae76
Akeksandr Motsjonov <soswow@gmail.com>
2012-03-09 20:20:33 +0200

    Code refactor

--- JS/ParticleFilter/script.coffee
+++ JS/ParticleFilter/script.coffee
@@ -1,7 +1,8 @@
-context = canvasHtml = null
-canvasSize = width: 500, height: 400
+context = canvasHtml = simulation = null #Semi-Globals placeholders
 
-root = exports ? this
+root = exports ? this #Global scope variable
+
+#Math function extraction
 random = Math.random
 pow = Math.pow
 sqrt = Math.sqrt
@@ -12,50 +13,121 @@ exp = Math.exp
 cos = Math.cos
 sin = Math.sin
 
-max = (arr) ->
-  result = -Number.MAX_VALUE
-  for el in arr
-    if el > result
-      result = el
-  result
+canvasSize = width: 500, height: 400
+shouldStop = false
 
-sum = (arr) ->
-  _sum = 0
-  for el in arr
-    _sum+=el
-  _sum
+root.reset = ->
+  #landmarks = [{x:100, y:100}, {x:200, y:350}]
+  #landmarks = [{x:100, y:100}]
+  canvasHtml.width = canvasHtml.width #clear the canvas
+  shouldStop = true
+  landmarks = [{x:100, y:100}, {x:200, y:350}, {x:400, y:370}, {x:270, y:90}, {x:350, y:200}]
+  N = 100
+  simulation = new Simulation(N, landmarks, null, 1, 0).initiateParticles().draw(false)
 
-randomGauss = (mu, sigma) ->
-  #Box–Muller transform implemtation. 2nd variant
-  #http://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
-  loop
-    x = 2 * random() - 1 # Random value from -1 to 1
-    y = 2 * random() - 1
-    s = pow(x,2) + pow(y, 2)
-    break unless s >= 1 || s == 0
+root.init = ->
+  prepareCanvas()
+  root.reset()
+root.pause = -> shouldStop = true
+root.step = -> animateStep()
+root.start = ->
+  shouldStop = false
+  requestAnimationFrame = window.requestAnimationFrame or window.mozRequestAnimationFrame or
+                          window.webkitRequestAnimationFrame or window.msRequestAnimationFrame
+  step = (timestamp) ->
+    animateStep()
+    unless shouldStop
+      requestAnimationFrame step
 
-  z = x * sqrt(-2 * log(s) / s)
-  #  console.log "Gauss (#{mu}, #{sigma}) -> #{res}"
-  mu + sigma * z
+  requestAnimationFrame step
 
-gauss = (mu, sigma, x) ->
-  # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
-  exp(-(pow((mu - x), 2) / pow(sigma, 2) / 2.0) / sqrt TWOPI * pow(sigma, 2))
+animateStep = ->
+  canvasHtml.width = canvasHtml.width #clear the canvas
+  simulation.step(randomGauss(0, pi/20)).draw()
 
-landmarks = [{x:100, y:100}, {x:200, y:350}, {x:400, y:370}, {x:270, y:90}, {x:350, y:200}]
-#landmarks = [{x:100, y:100}, {x:200, y:350}]
-#landmarks = [{x:100, y:100}]
-
-mod = (a, b) -> a % b + (if a < 0 then b else 0)
-
-prepare = ->
+prepareCanvas = ->
   canvasHtml = document.getElementById 'canvas'
   canvasHtml.width = canvasSize.width
   canvasHtml.height = canvasSize.height
   context = canvasHtml?.getContext? '2d'
 
-distance = (from, to) ->
-  sqrt (pow((from.x - to.x), 2) + pow((from.y - to.y), 2))
+class Simulation
+  constructor: (particlesNum, landmarks, noiseConfig, initMoveBy=1, initMakeTurn=0)->
+    @N = particlesNum
+    @landmarks = landmarks
+    @myrobot = new Robot().setColor 'red'
+    @myrobot.set_noise(noiseConfig) if noiseConfig
+    @particles = []
+    @moveBy = initMoveBy
+    @makeTurn = initMakeTurn
+
+  initiateParticles: (noiseConfig, color='gray') ->
+    noiseConfig or= forward_noise: 0.05, turn_noise: pi/20, sense_noise: 5
+    @particles = (new Robot().setColor(color).set_noise noiseConfig for i in [0..@N])
+    @
+
+  draw: (isRobotFirst=true) ->
+    if isRobotFirst
+      @myrobot.draw(10)
+    @drawLandmarks()
+    for particle,i in @particles
+      particle.draw(5)
+    unless isRobotFirst
+      @myrobot.draw(10)
+    @
+
+  drawLandmarks: (withLabels=false) ->
+    size = 10 / 2
+    context.strokeStyle = "black"
+    for landmark,i in @landmarks
+      context.beginPath()
+      x = landmark.x + 0.5
+      y = landmark.y + 0.5
+      context.moveTo x, y - size
+      context.lineTo x, y + size
+      context.moveTo x - size, y
+      context.lineTo x + size, y
+      if withLabels
+        context.font = 'normal 8px'
+        context.strokeStyle = 'black'
+        context.fillText i, x+8.5, y+3.5
+      context.closePath()
+      context.stroke()
+    @
+
+  step: (makeTurn=@makeTurn, moveBy=@moveBy)->
+#    makeTurn = randomGauss(0, pi/20)
+
+    # Move and Sense
+    @myrobot = @myrobot.move(makeTurn, moveBy, true)
+    Z = @myrobot.sense(@landmarks)
+
+    # Move particles with same actions as main robot
+    particles = (p.move makeTurn, moveBy, true for p in @particles)
+
+    weights = []
+    for p, i in particles
+      #console.log i
+      weights.push p.measurement_prob(Z, @landmarks)
+
+#    weightsSum = sum(weights)
+#    weightsNormalized = (w / weightsSum for w in weights)
+
+    #Resampling stange
+    resampledParticles = []
+    index = Math.round(random() * @N)
+    beta = 0
+    mw = max(weights)
+    for i in [0..@N]
+      beta += random() * 2.0 * mw
+      while beta > weights[index]
+        beta -= weights[index]
+        index = (index + 1) % @N
+      resampledParticles.push particles[index]
+
+    @particles = resampledParticles
+    @.draw false
+
 
 class Robot
   constructor: ->
@@ -82,16 +154,16 @@ class Robot
 
   setColor: (@color) -> @
 
-  set_noise: (new_f_noise, new_t_noise, new_s_noise) ->
-    @forward_noise = new_f_noise
-    @turn_noise    = new_t_noise
-    @sense_noise   = new_s_noise
+  set_noise: (noiseConfig) ->
+    @forward_noise = noiseConfig.forward_noise
+    @turn_noise = noiseConfig.turn_noise
+    @sense_noise = noiseConfig.sense_noise
     @
 
-  sense: ->
+  sense: (landmarks) ->
     distance(@, lendmark) + randomGauss(0, @sense_noise) for lendmark in landmarks
 
-  move: (turn, forward) ->
+  move: (turn, forward, makeNew=false) ->
     if forward < 0
       throw 'Robot cant move backwards'
 
@@ -106,13 +178,17 @@ class Robot
     x = mod x, canvasSize.width    # cyclic truncate
     y = mod y, canvasSize.height
 
-    # set particle
-    res = new Robot() #TODO REcheck this.
-    res.set x, y, orientation, @color
-    res.set_noise @forward_noise, @turn_noise, @sense_noise
-    res
+    if makeNew
+      robot = if makeNew then new Robot().set_noise({
+        forward_noise: @forward_noise,
+        turn_noise: @turn_noise,
+        sense_noise: @sense_noise
+      })
+    else
+      robot = @
+    robot.set x, y, orientation, @color
 
-  measurement_prob: (measurement) ->
+  measurement_prob: (measurement, landmarks) ->
     # calculates how likely a measurement should be
     probs = 1.0
     for landmark, i in landmarks
@@ -142,115 +218,41 @@ class Robot
     context.strokeStyle = 'black'
     context.fillText text, @x+8.5, @y+3.5
 
-drawLandmarks = ->
-  size = 10 / 2
-  context.strokeStyle = "black"
-  for landmark,i in landmarks
-    context.beginPath()
-    x = landmark.x + 0.5
-    y = landmark.y + 0.5
-    context.moveTo x, y - size
-    context.lineTo x, y + size
-    context.moveTo x - size, y
-    context.lineTo x + size, y
-    context.font = 'normal 8px'
-    context.strokeStyle = 'black'
-    context.fillText i, x+8.5, y+3.5
-    context.closePath()
-    context.stroke()
 
-run = ->
-  #Main robot To localize
-  myrobot = new Robot().setColor 'red'
+#Util methods implementation
+max = (arr) ->
+  result = -Number.MAX_VALUE
+  for el in arr
+    if el > result
+      result = el
+  result
 
-  # Draw initial robot possition
-#  myrobot.draw(10, 'rgb(255,100,100)')
+sum = (arr) ->
+  _sum = 0
+  for el in arr
+    _sum+=el
+  _sum
 
-  # Robot Actions
-  moveBy = 1
-  makeTurn = 0
+randomGauss = (mu, sigma) ->
+  #Box–Muller transform implemtation. 2nd variant
+  #http://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+  loop
+    x = 2 * random() - 1 # Random value from -1 to 1
+    y = 2 * random() - 1
+    s = pow(x,2) + pow(y, 2)
+    break unless s >= 1 || s == 0
 
-  T = 25000
-  N = 200
-
-  # Particles set creation
-  particles = (new Robot().setColor('gray').set_noise 0.05, pi/20, 5 for i in [0..N])
-
-  myrobot.draw(10)
-  drawLandmarks()
-  for particle,i in particles
-    particle.draw(5)
-
-  root.moveItNow = ->
-    makeTurn = randomGauss(0, pi/20)
-    drawLandmarks()
-
-    # Move and Sense
-    myrobot = myrobot.move(makeTurn, moveBy)
-    Z = myrobot.sense()
-    #console.log Z
-
-    # Draw initial particles possition
-  #  (particle.draw(5, 'gray') for particle in particles)
-
-    # Move particles with same actions as main robot
-    particles = (p.move makeTurn, moveBy for p in particles)
-
-    # Draw particles after move
-  #  (particle.draw(5) for particle in particles)
-
-    weights = []
-    for p, i in particles
-      #console.log i
-      weights.push p.measurement_prob(Z)
-
-    weightsSum = sum(weights)
-    weightsNormalized = (w / weightsSum for w in weights)
-
-    #Resampling stange
-    resampledParticles = []
-    index = Math.round(random() * N)
-    beta = 0
-    mw = max(weights)
-    for i in [0..N]
-      beta += random() * 2.0 * mw
-      while beta > weights[index]
-        beta -= weights[index]
-        index = (index + 1) % N
-      resampledParticles.push particles[index]
-
-    particles = resampledParticles
-
-    for particle,i in particles
-      particle.draw(5)
-
-    # Draw robot after move
-    myrobot.draw(10)
-
-shouldStop = false
-root.stop = ->
-  shouldStop = true
-
-root.init = ->
-  prepare()
-  run()
-
-root.start = ->
-  i = 0
-  cycle = ->
-    canvasHtml.width = canvasSize.width
-    root.moveItNow()
-
-  requestAnimationFrame = window.requestAnimationFrame or window.mozRequestAnimationFrame or
-                          window.webkitRequestAnimationFrame or window.msRequestAnimationFrame
-
-  start = Date.now()
-  step = (timestamp) ->
-    progress = timestamp - start
-    cycle()
-    unless shouldStop
-      requestAnimationFrame step
-
-  requestAnimationFrame step
+  z = x * sqrt(-2 * log(s) / s)
+  #  console.log "Gauss (#{mu}, #{sigma}) -> #{res}"
+  mu + sigma * z
 
 
+gauss = (mu, sigma, x) ->
+  # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+  exp(-(pow((mu - x), 2) / pow(sigma, 2) / 2.0) / sqrt TWOPI * pow(sigma, 2))
+
+#Modulo function which gives mod(-3, 10) == 7 (not -3 as in pure JS)
+mod = (a, b) -> a % b + (if a < 0 then b else 0)
+
+distance = (from, to) ->
+  sqrt (pow((from.x - to.x), 2) + pow((from.y - to.y), 2))
--- JS/ParticleFilter/script.js
+++ JS/ParticleFilter/script.js
@@ -1,12 +1,7 @@
 (function() {
-  var Robot, TWOPI, canvasHtml, canvasSize, context, cos, distance, drawLandmarks, exp, gauss, landmarks, log, max, mod, pi, pow, prepare, random, randomGauss, root, run, shouldStop, sin, sqrt, sum;
+  var Robot, Simulation, TWOPI, animateStep, canvasHtml, canvasSize, context, cos, distance, exp, gauss, log, max, mod, pi, pow, prepareCanvas, random, randomGauss, root, shouldStop, simulation, sin, sqrt, sum;
 
-  context = canvasHtml = null;
-
-  canvasSize = {
-    width: 500,
-    height: 400
-  };
+  context = canvasHtml = simulation = null;
 
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
@@ -28,75 +23,188 @@
 
   sin = Math.sin;
 
-  max = function(arr) {
-    var el, result, _i, _len;
-    result = -Number.MAX_VALUE;
-    for (_i = 0, _len = arr.length; _i < _len; _i++) {
-      el = arr[_i];
-      if (el > result) result = el;
-    }
-    return result;
+  canvasSize = {
+    width: 500,
+    height: 400
   };
 
-  sum = function(arr) {
-    var el, _i, _len, _sum;
-    _sum = 0;
-    for (_i = 0, _len = arr.length; _i < _len; _i++) {
-      el = arr[_i];
-      _sum += el;
-    }
-    return _sum;
+  shouldStop = false;
+
+  root.reset = function() {
+    var N, landmarks;
+    canvasHtml.width = canvasHtml.width;
+    shouldStop = true;
+    landmarks = [
+      {
+        x: 100,
+        y: 100
+      }, {
+        x: 200,
+        y: 350
+      }, {
+        x: 400,
+        y: 370
+      }, {
+        x: 270,
+        y: 90
+      }, {
+        x: 350,
+        y: 200
+      }
+    ];
+    N = 100;
+    return simulation = new Simulation(N, landmarks, null, 1, 0).initiateParticles().draw(false);
   };
 
-  randomGauss = function(mu, sigma) {
-    var s, x, y, z;
-    while (true) {
-      x = 2 * random() - 1;
-      y = 2 * random() - 1;
-      s = pow(x, 2) + pow(y, 2);
-      if (!(s >= 1 || s === 0)) break;
-    }
-    z = x * sqrt(-2 * log(s) / s);
-    return mu + sigma * z;
+  root.init = function() {
+    prepareCanvas();
+    return root.reset();
   };
 
-  gauss = function(mu, sigma, x) {
-    return exp(-(pow(mu - x, 2) / pow(sigma, 2) / 2.0) / sqrt(TWOPI * pow(sigma, 2)));
+  root.pause = function() {
+    return shouldStop = true;
   };
 
-  landmarks = [
-    {
-      x: 100,
-      y: 100
-    }, {
-      x: 200,
-      y: 350
-    }, {
-      x: 400,
-      y: 370
-    }, {
-      x: 270,
-      y: 90
-    }, {
-      x: 350,
-      y: 200
-    }
-  ];
-
-  mod = function(a, b) {
-    return a % b + (a < 0 ? b : 0);
+  root.step = function() {
+    return animateStep();
   };
 
-  prepare = function() {
+  root.start = function() {
+    var requestAnimationFrame, step;
+    shouldStop = false;
+    requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    step = function(timestamp) {
+      animateStep();
+      if (!shouldStop) return requestAnimationFrame(step);
+    };
+    return requestAnimationFrame(step);
+  };
+
+  animateStep = function() {
+    canvasHtml.width = canvasHtml.width;
+    return simulation.step(randomGauss(0, pi / 20)).draw();
+  };
+
+  prepareCanvas = function() {
     canvasHtml = document.getElementById('canvas');
     canvasHtml.width = canvasSize.width;
     canvasHtml.height = canvasSize.height;
     return context = canvasHtml != null ? typeof canvasHtml.getContext === "function" ? canvasHtml.getContext('2d') : void 0 : void 0;
   };
 
-  distance = function(from, to) {
-    return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2));
-  };
+  Simulation = (function() {
+
+    function Simulation(particlesNum, landmarks, noiseConfig, initMoveBy, initMakeTurn) {
+      if (initMoveBy == null) initMoveBy = 1;
+      if (initMakeTurn == null) initMakeTurn = 0;
+      this.N = particlesNum;
+      this.landmarks = landmarks;
+      this.myrobot = new Robot().setColor('red');
+      if (noiseConfig) this.myrobot.set_noise(noiseConfig);
+      this.particles = [];
+      this.moveBy = initMoveBy;
+      this.makeTurn = initMakeTurn;
+    }
+
+    Simulation.prototype.initiateParticles = function(noiseConfig, color) {
+      var i;
+      if (color == null) color = 'gray';
+      noiseConfig || (noiseConfig = {
+        forward_noise: 0.05,
+        turn_noise: pi / 20,
+        sense_noise: 5
+      });
+      this.particles = (function() {
+        var _ref, _results;
+        _results = [];
+        for (i = 0, _ref = this.N; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+          _results.push(new Robot().setColor(color).set_noise(noiseConfig));
+        }
+        return _results;
+      }).call(this);
+      return this;
+    };
+
+    Simulation.prototype.draw = function(isRobotFirst) {
+      var i, particle, _len, _ref;
+      if (isRobotFirst == null) isRobotFirst = true;
+      if (isRobotFirst) this.myrobot.draw(10);
+      this.drawLandmarks();
+      _ref = this.particles;
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        particle = _ref[i];
+        particle.draw(5);
+      }
+      if (!isRobotFirst) this.myrobot.draw(10);
+      return this;
+    };
+
+    Simulation.prototype.drawLandmarks = function(withLabels) {
+      var i, landmark, size, x, y, _len, _ref;
+      if (withLabels == null) withLabels = false;
+      size = 10 / 2;
+      context.strokeStyle = "black";
+      _ref = this.landmarks;
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        landmark = _ref[i];
+        context.beginPath();
+        x = landmark.x + 0.5;
+        y = landmark.y + 0.5;
+        context.moveTo(x, y - size);
+        context.lineTo(x, y + size);
+        context.moveTo(x - size, y);
+        context.lineTo(x + size, y);
+        if (withLabels) {
+          context.font = 'normal 8px';
+          context.strokeStyle = 'black';
+          context.fillText(i, x + 8.5, y + 3.5);
+        }
+        context.closePath();
+        context.stroke();
+      }
+      return this;
+    };
+
+    Simulation.prototype.step = function(makeTurn, moveBy) {
+      var Z, beta, i, index, mw, p, particles, resampledParticles, weights, _len, _ref;
+      if (makeTurn == null) makeTurn = this.makeTurn;
+      if (moveBy == null) moveBy = this.moveBy;
+      this.myrobot = this.myrobot.move(makeTurn, moveBy, true);
+      Z = this.myrobot.sense(this.landmarks);
+      particles = (function() {
+        var _i, _len, _ref, _results;
+        _ref = this.particles;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          p = _ref[_i];
+          _results.push(p.move(makeTurn, moveBy, true));
+        }
+        return _results;
+      }).call(this);
+      weights = [];
+      for (i = 0, _len = particles.length; i < _len; i++) {
+        p = particles[i];
+        weights.push(p.measurement_prob(Z, this.landmarks));
+      }
+      resampledParticles = [];
+      index = Math.round(random() * this.N);
+      beta = 0;
+      mw = max(weights);
+      for (i = 0, _ref = this.N; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        beta += random() * 2.0 * mw;
+        while (beta > weights[index]) {
+          beta -= weights[index];
+          index = (index + 1) % this.N;
+        }
+        resampledParticles.push(particles[index]);
+      }
+      this.particles = resampledParticles;
+      return this.draw(false);
+    };
+
+    return Simulation;
+
+  })();
 
   Robot = (function() {
 
@@ -132,14 +240,14 @@
       return this;
     };
 
-    Robot.prototype.set_noise = function(new_f_noise, new_t_noise, new_s_noise) {
-      this.forward_noise = new_f_noise;
-      this.turn_noise = new_t_noise;
-      this.sense_noise = new_s_noise;
+    Robot.prototype.set_noise = function(noiseConfig) {
+      this.forward_noise = noiseConfig.forward_noise;
+      this.turn_noise = noiseConfig.turn_noise;
+      this.sense_noise = noiseConfig.sense_noise;
       return this;
     };
 
-    Robot.prototype.sense = function() {
+    Robot.prototype.sense = function(landmarks) {
       var lendmark, _i, _len, _results;
       _results = [];
       for (_i = 0, _len = landmarks.length; _i < _len; _i++) {
@@ -149,8 +257,9 @@
       return _results;
     };
 
-    Robot.prototype.move = function(turn, forward) {
-      var dist, orientation, res, x, y;
+    Robot.prototype.move = function(turn, forward, makeNew) {
+      var dist, orientation, robot, x, y;
+      if (makeNew == null) makeNew = false;
       if (forward < 0) throw 'Robot cant move backwards';
       orientation = this.orientation + turn + randomGauss(0.0, this.turn_noise);
       orientation = mod(orientation, TWOPI);
@@ -159,13 +268,19 @@
       y = this.y + sin(orientation) * dist;
       x = mod(x, canvasSize.width);
       y = mod(y, canvasSize.height);
-      res = new Robot();
-      res.set(x, y, orientation, this.color);
-      res.set_noise(this.forward_noise, this.turn_noise, this.sense_noise);
-      return res;
+      if (makeNew) {
+        robot = makeNew ? new Robot().set_noise({
+          forward_noise: this.forward_noise,
+          turn_noise: this.turn_noise,
+          sense_noise: this.sense_noise
+        }) : void 0;
+      } else {
+        robot = this;
+      }
+      return robot.set(x, y, orientation, this.color);
     };
 
-    Robot.prototype.measurement_prob = function(measurement) {
+    Robot.prototype.measurement_prob = function(measurement, landmarks) {
       var dist, i, landmark, pp, prob, probs, _len;
       probs = 1.0;
       for (i = 0, _len = landmarks.length; i < _len; i++) {
@@ -204,128 +319,48 @@
 
   })();
 
-  drawLandmarks = function() {
-    var i, landmark, size, x, y, _len, _results;
-    size = 10 / 2;
-    context.strokeStyle = "black";
-    _results = [];
-    for (i = 0, _len = landmarks.length; i < _len; i++) {
-      landmark = landmarks[i];
-      context.beginPath();
-      x = landmark.x + 0.5;
-      y = landmark.y + 0.5;
-      context.moveTo(x, y - size);
-      context.lineTo(x, y + size);
-      context.moveTo(x - size, y);
-      context.lineTo(x + size, y);
-      context.font = 'normal 8px';
-      context.strokeStyle = 'black';
-      context.fillText(i, x + 8.5, y + 3.5);
-      context.closePath();
-      _results.push(context.stroke());
+  max = function(arr) {
+    var el, result, _i, _len;
+    result = -Number.MAX_VALUE;
+    for (_i = 0, _len = arr.length; _i < _len; _i++) {
+      el = arr[_i];
+      if (el > result) result = el;
     }
-    return _results;
+    return result;
   };
 
-  run = function() {
-    var N, T, i, makeTurn, moveBy, myrobot, particle, particles, _len;
-    myrobot = new Robot().setColor('red');
-    moveBy = 1;
-    makeTurn = 0;
-    T = 25000;
-    N = 200;
-    particles = (function() {
-      var _results;
-      _results = [];
-      for (i = 0; 0 <= N ? i <= N : i >= N; 0 <= N ? i++ : i--) {
-        _results.push(new Robot().setColor('gray').set_noise(0.05, pi / 20, 5));
-      }
-      return _results;
-    })();
-    myrobot.draw(10);
-    drawLandmarks();
-    for (i = 0, _len = particles.length; i < _len; i++) {
-      particle = particles[i];
-      particle.draw(5);
+  sum = function(arr) {
+    var el, _i, _len, _sum;
+    _sum = 0;
+    for (_i = 0, _len = arr.length; _i < _len; _i++) {
+      el = arr[_i];
+      _sum += el;
     }
-    return root.moveItNow = function() {
-      var Z, beta, i, index, mw, p, particle, resampledParticles, w, weights, weightsNormalized, weightsSum, _len2, _len3;
-      makeTurn = randomGauss(0, pi / 20);
-      drawLandmarks();
-      myrobot = myrobot.move(makeTurn, moveBy);
-      Z = myrobot.sense();
-      particles = (function() {
-        var _i, _len2, _results;
-        _results = [];
-        for (_i = 0, _len2 = particles.length; _i < _len2; _i++) {
-          p = particles[_i];
-          _results.push(p.move(makeTurn, moveBy));
-        }
-        return _results;
-      })();
-      weights = [];
-      for (i = 0, _len2 = particles.length; i < _len2; i++) {
-        p = particles[i];
-        weights.push(p.measurement_prob(Z));
-      }
-      weightsSum = sum(weights);
-      weightsNormalized = (function() {
-        var _i, _len3, _results;
-        _results = [];
-        for (_i = 0, _len3 = weights.length; _i < _len3; _i++) {
-          w = weights[_i];
-          _results.push(w / weightsSum);
-        }
-        return _results;
-      })();
-      resampledParticles = [];
-      index = Math.round(random() * N);
-      beta = 0;
-      mw = max(weights);
-      for (i = 0; 0 <= N ? i <= N : i >= N; 0 <= N ? i++ : i--) {
-        beta += random() * 2.0 * mw;
-        while (beta > weights[index]) {
-          beta -= weights[index];
-          index = (index + 1) % N;
-        }
-        resampledParticles.push(particles[index]);
-      }
-      particles = resampledParticles;
-      for (i = 0, _len3 = particles.length; i < _len3; i++) {
-        particle = particles[i];
-        particle.draw(5);
-      }
-      return myrobot.draw(10);
-    };
+    return _sum;
   };
 
-  shouldStop = false;
-
-  root.stop = function() {
-    return shouldStop = true;
+  randomGauss = function(mu, sigma) {
+    var s, x, y, z;
+    while (true) {
+      x = 2 * random() - 1;
+      y = 2 * random() - 1;
+      s = pow(x, 2) + pow(y, 2);
+      if (!(s >= 1 || s === 0)) break;
+    }
+    z = x * sqrt(-2 * log(s) / s);
+    return mu + sigma * z;
   };
 
-  root.init = function() {
-    prepare();
-    return run();
+  gauss = function(mu, sigma, x) {
+    return exp(-(pow(mu - x, 2) / pow(sigma, 2) / 2.0) / sqrt(TWOPI * pow(sigma, 2)));
   };
 
-  root.start = function() {
-    var cycle, i, requestAnimationFrame, start, step;
-    i = 0;
-    cycle = function() {
-      canvasHtml.width = canvasSize.width;
-      return root.moveItNow();
-    };
-    requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    start = Date.now();
-    step = function(timestamp) {
-      var progress;
-      progress = timestamp - start;
-      cycle();
-      if (!shouldStop) return requestAnimationFrame(step);
-    };
-    return requestAnimationFrame(step);
+  mod = function(a, b) {
+    return a % b + (a < 0 ? b : 0);
+  };
+
+  distance = function(from, to) {
+    return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2));
   };
 
 }).call(this);
