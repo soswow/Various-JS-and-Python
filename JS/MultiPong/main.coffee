137080a651e36b058acffd1f73c64d1a7260ca42
Akeksandr Motsjonov <soswow@gmail.com>
2012-06-08 08:02:20 +0300

    Continue on multipong

--- JS/MultiPong/main.coffee
+++ JS/MultiPong/main.coffee
@@ -1,13 +1,54 @@
+W = 500
+H = 500
+
+#canvasSize =
+#  width: 500
+#  height: 500
+
 $.ready ->
+  console.log "ready"
   game = new Game()
 
-class GameCanvas
+class Canvas
+  constructor: (game) ->
+    @state = game.state
+    @prepare()
+
+  prepare: ->
+    canvasHtml = $('#canvas')
+    canvasHtml.attr 'width', W
+    canvasHtml.attr 'height', H
+    @context = canvasHtml?.getContext? '2d'
+
+  repaint: ->
+    #TODO
+
+
+class State
   constructor: ->
-    @canvas = $("#canvas")
+    @initBall()
+    @initPlayers()
+
+  initBall: ->
+    vectorXRnd = (Math.random() * 50) - 25;
+    vectorYRnd = (Math.random() * 50) - 25;
+    @ball = pos: xy(W/2, H/2), vector: xy(W / 2 + vectorXRnd, H / 2 + vectorYRnd)
+
+  initPlayers: ->
+
+  update: ->
+
 
 class Game
   constructor: ->
-    console.log "init"
+    @state = new State(this)
+    @canvas = new Canvas(this)
+    @initHandlers()
 
-  redraw: ->
+  initHandlers: ->
+    $('body').on "mousemove", (e) =>
+      @state.update xy e.clientX, e.clientY
 
+  refresh: ->
+
+xy = (x,y) -> x:x, y:y
--- JS/MultiPong/main.js
+++ JS/MultiPong/main.js
@@ -1,31 +1,88 @@
 (function() {
-  var Game, GameCanvas;
+  var Canvas, Game, H, State, W, xy;
+
+  W = 500;
+
+  H = 500;
 
   $.ready(function() {
     var game;
+    console.log("ready");
     return game = new Game();
   });
 
-  GameCanvas = (function() {
+  Canvas = (function() {
 
-    function GameCanvas() {
-      this.canvas = $("#canvas");
+    function Canvas(game) {
+      this.state = game.state;
+      this.prepare();
     }
 
-    return GameCanvas;
+    Canvas.prototype.prepare = function() {
+      var canvasHtml;
+      canvasHtml = $('#canvas');
+      canvasHtml.attr('width', W);
+      canvasHtml.attr('height', H);
+      return this.context = canvasHtml != null ? typeof canvasHtml.getContext === "function" ? canvasHtml.getContext('2d') : void 0 : void 0;
+    };
+
+    Canvas.prototype.repaint = function() {};
+
+    return Canvas;
+
+  })();
+
+  State = (function() {
+
+    function State() {
+      this.initBall();
+      this.initPlayers();
+    }
+
+    State.prototype.initBall = function() {
+      var vectorXRnd, vectorYRnd;
+      vectorXRnd = (Math.random() * 50) - 25;
+      vectorYRnd = (Math.random() * 50) - 25;
+      return this.ball = {
+        pos: xy(W / 2, H / 2),
+        vector: xy(W / 2 + vectorXRnd, H / 2 + vectorYRnd)
+      };
+    };
+
+    State.prototype.initPlayers = function() {};
+
+    State.prototype.update = function() {};
+
+    return State;
 
   })();
 
   Game = (function() {
 
     function Game() {
-      console.log("init");
+      this.state = new State(this);
+      this.canvas = new Canvas(this);
+      this.initHandlers();
     }
 
-    Game.prototype.redraw = function() {};
+    Game.prototype.initHandlers = function() {
+      var _this = this;
+      return $('body').on("mousemove", function(e) {
+        return _this.state.update(xy(e.clientX, e.clientY));
+      });
+    };
+
+    Game.prototype.refresh = function() {};
 
     return Game;
 
   })();
+
+  xy = function(x, y) {
+    return {
+      x: x,
+      y: y
+    };
+  };
 
 }).call(this);
