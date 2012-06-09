3629bec95cc18c1c91b98872cae6a686739b125b
Akeksandr Motsjonov <soswow@gmail.com>
2012-06-09 21:45:30 +0300

    It's alive!!! (platform)

--- JS/MultiPong/main.coffee
+++ JS/MultiPong/main.coffee
@@ -1,13 +1,46 @@
-W = 500
-H = 500
+#Ask for those constants from the server
+INNER_SIDE = 500
+WALL_THICK = 5
+SIDE = INNER_SIDE + WALL_THICK * 2
+INIT_PLAYER_SIZE = 0.1 * INNER_SIDE
+HALF_WALL_THICK = WALL_THICK / 2
 
-#canvasSize =
-#  width: 500
-#  height: 500
-
-$.ready ->
+$ ->
   console.log "ready"
   game = new Game()
+  $("#button").bind 'click', -> game.initMainLoop()
+
+class Game
+  constructor: ->
+    console.log "constructor"
+    @state = new State()
+    @canvas = new Canvas(this)
+    @state.addPlayer(new Player())
+    @initHandlers()
+    @canvas.repaint()
+
+  initHandlers: ->
+    @canvas.el.bind "mousemove", (e) =>
+      @updateAndRepaint 0, e.offsetX
+
+  updateAndRepaint: (timeLeft, clientX) ->
+    @state.update timeLeft, clientX
+    @canvas.repaint()
+
+  initMainLoop: ->
+    console.log "initMainLoop"
+    shouldStop = false
+    requestAnimationFrame = window.requestAnimationFrame or window.mozRequestAnimationFrame or
+                            window.webkitRequestAnimationFrame or window.msRequestAnimationFrame
+    prevTimestamp = null
+    step = (timestamp) =>
+      prevTimestamp ?= timestamp
+      unless shouldStop
+        @updateAndRepaint timestamp - prevTimestamp
+        requestAnimationFrame step
+      prevTimestamp = timestamp
+    requestAnimationFrame step
+
 
 class Canvas
   constructor: (game) ->
@@ -15,40 +48,116 @@ class Canvas
     @prepare()
 
   prepare: ->
-    canvasHtml = $('#canvas')
-    canvasHtml.attr 'width', W
-    canvasHtml.attr 'height', H
-    @context = canvasHtml?.getContext? '2d'
+    @el = $('#canvas')
+    @el.attr 'width', SIDE
+    @el.attr 'height', SIDE
+    @context = @el[0]?.getContext? '2d'
+    console.log @context
 
   repaint: ->
+    @clearAll()
+    @drawWalls()
+    @drawBall()
+
+  clearAll: ->
+    @el.attr 'width', SIDE
+
+  drawWalls: ->
+    for [from, to] in @state.walls()
+      console.log from+"", to+""
+      @context.lineWidth = WALL_THICK
+      @context.beginPath()
+      @context.moveTo from.x, from.y
+      @context.lineTo to.x, to.y
+      @context.closePath()
+      @context.stroke()
+      @context.lineWidth = 1
+
+  drawBall: ->
     #TODO
 
 
+class Ball
+  constructor: (@pos, @angle, @speed) ->
+    unless @pos and @angle and @speed
+      @randomInit()
+
+  randomInit: ->
+    @pos = xy SIDE/2, SIDE/2
+    @angle = randomInRange 0, 360
+    @speed = randomInRange 5, 10
+
+  move: (walls) ->
+    #TODO
+
 class State
+  @playerIndexSideMap: ['bottom', 'top', 'right', 'left']
+
   constructor: ->
-    @initBall()
-    @initPlayers()
+    @ball = new Ball()
+    @players = (null for a in [1..4])
 
-  initBall: ->
-    vectorXRnd = (Math.random() * 50) - 25;
-    vectorYRnd = (Math.random() * 50) - 25;
-    @ball = pos: xy(W/2, H/2), vector: xy(W / 2 + vectorXRnd, H / 2 + vectorYRnd)
+  walls: ->
+    #Return all walls from current state:
+    # - active Player platforms
+    # - walls on place of inactive players
+    #TODO Identify what is wall and what is player
+    console.log "walls"
+    for side, i in State.playerIndexSideMap
+      [wallStart, wallEnd] =
+        if @players[i]
+          @players[i].getWallPosition()
+        else
+          switch side
+            when 'bottom' then [xy(0, SIDE-HALF_WALL_THICK), xy(SIDE, SIDE-HALF_WALL_THICK)]
+            when 'top'    then [xy(0, HALF_WALL_THICK), xy(SIDE, HALF_WALL_THICK)]
+            when 'right'  then [xy(SIDE-HALF_WALL_THICK, 0), xy(SIDE-HALF_WALL_THICK, SIDE)]
+            when 'left'   then [xy(HALF_WALL_THICK, 0), xy(HALF_WALL_THICK, SIDE)]
+            else [xy(0,0), xy(0,0)]
 
-  initPlayers: ->
+  addPlayer: (newPlayer) ->
+    #Find slot in array and put into first free
+    #Assign it's side accordint to slot
+    for player, i in @players
+      unless player
+        newPlayer.side = State.playerIndexSideMap[i]
+        return @players[i] = newPlayer
 
-  update: ->
+  update: (timeleft, clientX) ->
+#    console.log timeleft
+    if clientX
+      @players[0].move clientX
+
+class Player
+  constructor: (@name) ->
+    @side = State.playerIndexSideMap[0]
+    @size = INIT_PLAYER_SIZE
+    @pos = 0.5 #center point position in percents
+
+  getWallPosition: ->
+    #Return pair of points: start & end of the wall
+    wallCenter = (INNER_SIDE - @size) * @pos
+    [from, to] = [wallCenter+WALL_THICK, wallCenter + @size + WALL_THICK]
+    console.log this, wallCenter, from, to
+    switch @side
+      when 'bottom' then [xy(from, SIDE-HALF_WALL_THICK), xy(to, SIDE-HALF_WALL_THICK)]
+      when 'top'    then [xy(from, HALF_WALL_THICK), xy(to, HALF_WALL_THICK)]
+      when 'right'  then [xy(SIDE-HALF_WALL_THICK, from), xy(SIDE-HALF_WALL_THICK, to)]
+      when 'left'   then [xy(HALF_WALL_THICK, from), xy(HALF_WALL_THICK, to)]
+
+  move: (clientX) ->
+    clientX -= @size / 2
+    @pos = clientX / (INNER_SIDE - @size)
+    @pos = 1 if @pos > 1
+    @pos = 0 if @pos < 0
 
 
-class Game
-  constructor: ->
-    @state = new State(this)
-    @canvas = new Canvas(this)
-    @initHandlers()
+randomInRange = (from, to) -> Math.random() * (to - from) + from;
+degToRad = (deg) -> deg * (Math.PI / 180)
 
-  initHandlers: ->
-    $('body').on "mousemove", (e) =>
-      @state.update xy e.clientX, e.clientY
-
-  refresh: ->
-
-xy = (x,y) -> x:x, y:y
+xy = (x, y) -> new Point(x, y)
+class Point
+  constructor: (@x, @y) ->
+#  shifted: ->
+#    new Point(@x + W)
+  toString: -> "(#{@x}, #{@y})"
--- JS/MultiPong/main.js
+++ JS/MultiPong/main.js
@@ -1,15 +1,69 @@
 (function() {
-  var Canvas, Game, H, State, W, xy;
+  var Ball, Canvas, Game, HALF_WALL_THICK, INIT_PLAYER_SIZE, INNER_SIDE, Player, Point, SIDE, State, WALL_THICK, degToRad, randomInRange, xy;
 
-  W = 500;
+  INNER_SIDE = 500;
 
-  H = 500;
+  WALL_THICK = 5;
 
-  $.ready(function() {
+  SIDE = INNER_SIDE + WALL_THICK * 2;
+
+  INIT_PLAYER_SIZE = 0.1 * INNER_SIDE;
+
+  HALF_WALL_THICK = WALL_THICK / 2;
+
+  $(function() {
     var game;
     console.log("ready");
-    return game = new Game();
+    game = new Game();
+    return $("#button").bind('click', function() {
+      return game.initMainLoop();
+    });
   });
+
+  Game = (function() {
+
+    function Game() {
+      console.log("constructor");
+      this.state = new State();
+      this.canvas = new Canvas(this);
+      this.state.addPlayer(new Player());
+      this.initHandlers();
+      this.canvas.repaint();
+    }
+
+    Game.prototype.initHandlers = function() {
+      var _this = this;
+      return this.canvas.el.bind("mousemove", function(e) {
+        return _this.updateAndRepaint(0, e.offsetX);
+      });
+    };
+
+    Game.prototype.updateAndRepaint = function(timeLeft, clientX) {
+      this.state.update(timeLeft, clientX);
+      return this.canvas.repaint();
+    };
+
+    Game.prototype.initMainLoop = function() {
+      var prevTimestamp, requestAnimationFrame, shouldStop, step,
+        _this = this;
+      console.log("initMainLoop");
+      shouldStop = false;
+      requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+      prevTimestamp = null;
+      step = function(timestamp) {
+        if (prevTimestamp == null) prevTimestamp = timestamp;
+        if (!shouldStop) {
+          _this.updateAndRepaint(timestamp - prevTimestamp);
+          requestAnimationFrame(step);
+        }
+        return prevTimestamp = timestamp;
+      };
+      return requestAnimationFrame(step);
+    };
+
+    return Game;
+
+  })();
 
   Canvas = (function() {
 
@@ -19,70 +73,197 @@
     }
 
     Canvas.prototype.prepare = function() {
-      var canvasHtml;
-      canvasHtml = $('#canvas');
-      canvasHtml.attr('width', W);
-      canvasHtml.attr('height', H);
-      return this.context = canvasHtml != null ? typeof canvasHtml.getContext === "function" ? canvasHtml.getContext('2d') : void 0 : void 0;
+      var _ref;
+      this.el = $('#canvas');
+      this.el.attr('width', SIDE);
+      this.el.attr('height', SIDE);
+      this.context = (_ref = this.el[0]) != null ? typeof _ref.getContext === "function" ? _ref.getContext('2d') : void 0 : void 0;
+      return console.log(this.context);
     };
 
-    Canvas.prototype.repaint = function() {};
+    Canvas.prototype.repaint = function() {
+      this.clearAll();
+      this.drawWalls();
+      return this.drawBall();
+    };
+
+    Canvas.prototype.clearAll = function() {
+      return this.el.attr('width', SIDE);
+    };
+
+    Canvas.prototype.drawWalls = function() {
+      var from, to, _i, _len, _ref, _ref2, _results;
+      _ref = this.state.walls();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        _ref2 = _ref[_i], from = _ref2[0], to = _ref2[1];
+        console.log(from + "", to + "");
+        this.context.lineWidth = WALL_THICK;
+        this.context.beginPath();
+        this.context.moveTo(from.x, from.y);
+        this.context.lineTo(to.x, to.y);
+        this.context.closePath();
+        this.context.stroke();
+        _results.push(this.context.lineWidth = 1);
+      }
+      return _results;
+    };
+
+    Canvas.prototype.drawBall = function() {};
 
     return Canvas;
 
   })();
 
-  State = (function() {
+  Ball = (function() {
 
-    function State() {
-      this.initBall();
-      this.initPlayers();
+    function Ball(pos, angle, speed) {
+      this.pos = pos;
+      this.angle = angle;
+      this.speed = speed;
+      if (!(this.pos && this.angle && this.speed)) this.randomInit();
     }
 
-    State.prototype.initBall = function() {
-      var vectorXRnd, vectorYRnd;
-      vectorXRnd = (Math.random() * 50) - 25;
-      vectorYRnd = (Math.random() * 50) - 25;
-      return this.ball = {
-        pos: xy(W / 2, H / 2),
-        vector: xy(W / 2 + vectorXRnd, H / 2 + vectorYRnd)
-      };
+    Ball.prototype.randomInit = function() {
+      this.pos = xy(SIDE / 2, SIDE / 2);
+      this.angle = randomInRange(0, 360);
+      return this.speed = randomInRange(5, 10);
     };
 
-    State.prototype.initPlayers = function() {};
+    Ball.prototype.move = function(walls) {};
 
-    State.prototype.update = function() {};
+    return Ball;
+
+  })();
+
+  State = (function() {
+
+    State.playerIndexSideMap = ['bottom', 'top', 'right', 'left'];
+
+    function State() {
+      var a;
+      this.ball = new Ball();
+      this.players = (function() {
+        var _results;
+        _results = [];
+        for (a = 1; a <= 4; a++) {
+          _results.push(null);
+        }
+        return _results;
+      })();
+    }
+
+    State.prototype.walls = function() {
+      var i, side, wallEnd, wallStart, _len, _ref, _ref2, _results;
+      console.log("walls");
+      _ref = State.playerIndexSideMap;
+      _results = [];
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        side = _ref[i];
+        _results.push((_ref2 = (function() {
+          if (this.players[i]) {
+            return this.players[i].getWallPosition();
+          } else {
+            switch (side) {
+              case 'bottom':
+                return [xy(0, SIDE - HALF_WALL_THICK), xy(SIDE, SIDE - HALF_WALL_THICK)];
+              case 'top':
+                return [xy(0, HALF_WALL_THICK), xy(SIDE, HALF_WALL_THICK)];
+              case 'right':
+                return [xy(SIDE - HALF_WALL_THICK, 0), xy(SIDE - HALF_WALL_THICK, SIDE)];
+              case 'left':
+                return [xy(HALF_WALL_THICK, 0), xy(HALF_WALL_THICK, SIDE)];
+              default:
+                return [xy(0, 0), xy(0, 0)];
+            }
+          }
+        }).call(this), wallStart = _ref2[0], wallEnd = _ref2[1], _ref2));
+      }
+      return _results;
+    };
+
+    State.prototype.addPlayer = function(newPlayer) {
+      var i, player, _len, _ref;
+      _ref = this.players;
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        player = _ref[i];
+        if (!player) {
+          newPlayer.side = State.playerIndexSideMap[i];
+          return this.players[i] = newPlayer;
+        }
+      }
+    };
+
+    State.prototype.update = function(timeleft, clientX) {
+      if (clientX) return this.players[0].move(clientX);
+    };
 
     return State;
 
   })();
 
-  Game = (function() {
+  Player = (function() {
 
-    function Game() {
-      this.state = new State(this);
-      this.canvas = new Canvas(this);
-      this.initHandlers();
+    function Player(name) {
+      this.name = name;
+      this.side = State.playerIndexSideMap[0];
+      this.size = INIT_PLAYER_SIZE;
+      this.pos = 0.5;
     }
 
-    Game.prototype.initHandlers = function() {
-      var _this = this;
-      return $('body').on("mousemove", function(e) {
-        return _this.state.update(xy(e.clientX, e.clientY));
-      });
+    Player.prototype.getWallPosition = function() {
+      var from, to, wallCenter, _ref;
+      wallCenter = (INNER_SIDE - this.size) * this.pos;
+      _ref = [wallCenter + WALL_THICK, wallCenter + this.size + WALL_THICK], from = _ref[0], to = _ref[1];
+      console.log(this, wallCenter, from, to);
+      switch (this.side) {
+        case 'bottom':
+          return [xy(from, SIDE - HALF_WALL_THICK), xy(to, SIDE - HALF_WALL_THICK)];
+        case 'top':
+          return [xy(from, HALF_WALL_THICK), xy(to, HALF_WALL_THICK)];
+        case 'right':
+          return [xy(SIDE - HALF_WALL_THICK, from), xy(SIDE - HALF_WALL_THICK, to)];
+        case 'left':
+          return [xy(HALF_WALL_THICK, from), xy(HALF_WALL_THICK, to)];
+      }
     };
 
-    Game.prototype.refresh = function() {};
+    Player.prototype.move = function(clientX) {
+      clientX -= this.size / 2;
+      this.pos = clientX / (INNER_SIDE - this.size);
+      if (this.pos > 1) this.pos = 1;
+      if (this.pos < 0) return this.pos = 0;
+    };
 
-    return Game;
+    return Player;
 
   })();
 
-  xy = function(x, y) {
-    return {
-      x: x,
-      y: y
-    };
+  randomInRange = function(from, to) {
+    return Math.random() * (to - from) + from;
   };
+
+  degToRad = function(deg) {
+    return deg * (Math.PI / 180);
+  };
+
+  xy = function(x, y) {
+    return new Point(x, y);
+  };
+
+  Point = (function() {
+
+    function Point(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    Point.prototype.toString = function() {
+      return "(" + this.x + ", " + this.y + ")";
+    };
+
+    return Point;
+
+  })();
 
 }).call(this);
