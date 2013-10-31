51db44aa809c60154ea08abc6b26939800fc7612
Aleksandr <aleksandr@toggl.com>
2013-10-31 13:30:49 +0200

    No workers needed you throw away setTimeout

--- JS/CanvasExperiments/app/main.js
+++ JS/CanvasExperiments/app/main.js
@@ -29,24 +29,32 @@ var buffer = [], i, j, k=0;
 //  for (j = 0; j < max.h * max.w * 4; buffer[i].data[j++] = Math.round(Math.random() * 155 + i));
 //}
 
-console.time("Create buffer");
-var workers = [];
-var workersNum = 4;
-for (i = 0; i < workersNum; i++) {
-  workers.push(new Worker("make-data.js"));
-  workers[i].onmessage = function (oEvent) {
-    var imageData = ctx.createImageData(max.w, max.h);
-    imageData.data.set(oEvent.data);
-    buffer.push(imageData);
-    if (buffer.length == 100) {
-      console.timeEnd("Create buffer");
-    }
-  };
-}
+//console.time("Create buffer");
+//var workers = [];
+//var workersNum = 4;
+var num = 100;
+//for (i = 0; i < workersNum; i++) {
+//  workers.push(new Worker("make-data.js"));
+//  workers[i].onmessage = function (oEvent) {
+//    console.log('hello');
+//    var imageData = ctx.createImageData(max.w, max.h);
+//    imageData.data.set(oEvent.data);
+//    buffer.push(imageData);
+//    if (buffer.length == num) {
+//      console.timeEnd("Create buffer");
+//    }
+//  };
+//}
 
-for (i = 0; i < 100; i++) {
-  workers[i % workersNum].postMessage(max);
-}
+//for (i = 0; i < num; i++) {
+//  var data = new Uint8ClampedArray(max.h * max.w * 4);
+//  for (j = 0; j < max.h * max.w * 4; data[j++] = Math.round(Math.random() * 255));
+//  var imageData = ctx.createImageData(max.w, max.h);
+//  imageData.data.set(data);
+//  buffer.push(imageData);
+//  workers[i % workersNum].postMessage(max);
+//}
+//console.timeEnd("Create buffer");
 
 
 //var imageData = canvas.getContext('2d').createImageData(width, height);
@@ -54,17 +62,25 @@ for (i = 0; i < 100; i++) {
 
 //var imageData = ctx.createImageData(max.w, max.h);
 var stop = false;
+var drawNewImage = function(){
+//  var data = new Uint8ClampedArray(max.h * max.w * 4);
+  var imageData = ctx.createImageData(max.w, max.h);
+  for (j = 0; j < max.h * max.w * 4; imageData.data[j++] = Math.round(Math.random() * 255));
+  ctx.putImageData(imageData, 0, 0, 0, 0, max.w, max.h);
+};
+
 step = function () {
-  requestAnimationFrame(function () {
+  console.timeStamp("Step Start");
 //    var imageData = ctx.createImageData(max.w, max.h);
 //    for (i = 0; i < max.h * max.w * 4; imageData.data[i++] = Math.round(Math.random() * 255)) {}
-    if(k>=buffer.length) k=0;
-    ctx.putImageData(buffer[k++], 0, 0, 0, 0, max.w, max.h);
-    $log.innerText = k;
-    if (!stop) {
-      setTimeout(step, 6);
-    }
-  });
+//  if(k>=buffer.length) k=0;
+  drawNewImage();
+//  $log.innerText = k;
+  console.timeStamp("Before next frame");
+  if (!stop) {
+    requestAnimationFrame(step);
+  }
+  console.timeStamp("Step End");
 };
 //step();
 
--- JS/CanvasExperiments/app/make-data.js
+++ JS/CanvasExperiments/app/make-data.js
@@ -1,9 +1,6 @@
 onmessage = function (oEvent) {
-//  postMessage("Hi " + oEvent.data);
-//  var ctx = oEvent.data;
-//  var imageData = ctx.createImageData(max.w, max.h);
   var max = oEvent.data;
-  var data = new Uint8ClampedArray();
-  for (var j = 0; j < max.h * max.w * 4; data[j++] = Math.round(Math.random() * 155));
+  var data = new Uint8ClampedArray(max.h * max.w * 4);
+  for (var j = 0; j < max.h * max.w * 4; data[j++] = Math.round(Math.random() * 255));
   postMessage(data);
 };
